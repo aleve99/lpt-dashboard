3f0cdea9e1313edc51c70463ea106ff0a817488c
aleve99 <alessiovecchiet@gmail.com>
2024-06-10 19:05:14 +0200

    Added tenderize support

--- main.py
+++ main.py
@@ -31,17 +31,13 @@ for filename in CONFIG_PATH.iterdir():
 def main():
    
     provider_arb = Web3.HTTPProvider(config["endpoint"]["arbitrum"])
-    #provider_eth = Web3.HTTPProvider(config["endpoint"]["ethereum"])
 
     w3_arb = Web3(provider=provider_arb)
-    #w3_eth = Web3(provider=provider_eth)
-
-    #arb_latest_block = w3_arb.eth.get_block_number()
-    #eth_latest_block = w3_eth.eth.get_block_number()
 
     bonding_manager_contract: Contract  = w3_arb.eth.contract(config["contract"]["bonding_manager"], abi=abi.BONDING_MANAGER)
     round_manager_contract: Contract  = w3_arb.eth.contract(config["contract"]["rounds_manager"], abi=abi.ROUND_MANAGER)
     lpt_token_contract: Contract = w3_arb.eth.contract(config["contract"]["lpt_token"], abi=abi.ERC_20)
+    tlpt_token_contract: Contract = w3_arb.eth.contract(config["contract"]["tlpt_token"], abi=abi.ERC_20)
 
     lpt_decimals = lpt_token_contract.functions.decimals().call()
 
@@ -59,11 +55,21 @@ def main():
         "fees": []
     }
 
-    for event in tqdm(new_round_events, desc="LPT rounds scan"):
+    for event in tqdm(new_round_events, desc="Stake scraping"):
         round = event['args']['round']
         block = event['blockNumber']
         
-        pending_stake = bonding_manager_contract.functions.pendingStake(config["delegator"], 0).call(block_identifier=block)
+        status = bonding_manager_contract.functions.delegatorStatus(config["delegator"]).call(block_identifier=block)
+
+        if status == 1:
+            pending_stake = bonding_manager_contract.functions.pendingStake(config["delegator"], 0).call(block_identifier=block)
+        else:
+            lock_id = bonding_manager_contract.functions.getDelegator(config["delegator"]).call(block_identifier=block)[-1] - 1
+            pending_stake = bonding_manager_contract.functions.getDelegatorUnbondingLock(config["delegator"], lock_id).call(block_identifier=block)[0]
+            
+            if pending_stake == 0:
+                pending_stake = tlpt_token_contract.functions.balanceOf(config['delegator']).call(block_identifier=block)
+
         pending_fees = bonding_manager_contract.functions.pendingFees(config["delegator"], 0).call(block_identifier=block)
 
         stake_fees_data["round"].append(round)
@@ -72,6 +78,8 @@ def main():
     
     df = DataFrame(stake_fees_data)
 
+    with open("stake.csv", "w") as file:
+        df.to_csv(file, sep=",", index=False)
 
 if __name__ == "__main__":
     try:
